05a949a1aae6292a43e0ae7526e59c2e6f9dc1b8
Rohit.singh <rohit.singh@172-11-6-49.lightspeed.jcvlfl.sbcglobal.net>
2017-02-13 16:48:34 +0530

    Rohit

---
+++ CarthageiOS-Demo/CarthageiOS-Demo/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  CarthageiOS-Demo
+//
+//  Created by Rohit.Singh on 13/02/17.
+//  Copyright © 2017 Rohit.Singh. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        Alamofire.request("https://httpbin.org/get").responseJSON { response in
+            print(response.request as Any)  // original URL request
+            print(response.response as Any) // HTTP URL response
+            print(response.data as Any)     // server data
+            print(response.result)   // result of response serialization
+            
+            if let JSON = response.result.value {
+                print("JSON: \(JSON)")
+            }
+        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
